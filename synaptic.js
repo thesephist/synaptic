623d4de068a2ffd8022d6e167645db70d6a4caf6
Linus Lee <100479lee@gmail.com>
2015-11-27 23:15:54 -0500

    Overhauled runtime structure

--- synaptic.js
+++ synaptic.js
@@ -1,25 +1,35 @@
 #!/usr/bin/env node
-/* Project Synaptic 2.0: General Intelligence Project
- */
 
-/* project outline
+/* Project Synaptic 2.0: General Intelligence Project
+ * Copyright Linus Lee 2015, All rights reserved.
+ *
+ * For inquires please contact linus@thelifelongtraveler.com
+ * * * * *
+ * Project outline
  *
  * Event-driven, massively parallel system
+ * Changes in N.a: runs after an N.fire, then reflects any learning by filling from ambient right away over the next few intervals
+ * There is always a nonzero, exponentially decaying distribution of neurotransmitters
  */
 
 // each neuron index associated with its stimulus, KEY
 var keys = {
-        0: "a", // this is the first neuron in any network
-        1: "0",
-        2: "1"
+        0: "0",
+        1: "1",
+        2: "f"
 };
+
 var rkeys = {}, 
     scale = 0;
 
 for (j in keys){
-  if(!Object.prototype.hasOwnProperty.call(keys, j)){continue}
-  rkeys[keys[j]] = j;
-  scale+=1;
+    if (!Object.prototype.hasOwnProperty.call(keys, j)) {
+      continue;
+    }
+
+    rkeys[keys[j]] = j;
+    
+    scale += 1;
 }
 
 // init variables
@@ -29,114 +39,137 @@ var supervised = true, // if true, prompt for each input
     mind = [], // array of all neurons
     pace = 1.3, // learning pace, with 1 being static; nothing learned
     depreciation_c = 0.8, // how fast individual neurons (and thus the system) loses neurotransmitters
-    variation = 0.3, // some preset value
-    n_dist = 0.5; // neurotransmitter distribution, a float (0,1]
+    variation = 0.05, // some preset value
+    n_dist = 1.0; // neurotransmitter distribution, a float around the value of 1
+
 
 // define which neurons are connected to which
 var map = [
-    "000",
-    "001",
-    "010"
-]
+    "011",
+    "101",
+    "000"
+];
  
 // define class Neuron (N)
 function N (index, key) {
     // basic properties
-    this.state = 0; // integer neuron states
+    this.state = 0.0; // float neuron states
     this.index = index; // numerical index in system, starts at 0
     this.key = key; // responds to KEY or activates response
     this.s = []; // strengths
     this.c = map[index].split(""); // connected neurons log
-    this.h = parseFloat(0); // history
 }
 
 N.prototype.init = function(){
+    
+    // how many neurons is this connected to?
     mod_length = 0;
+    
     this.c.forEach(function(link){
-        if(link=='1'){mod_length+=1;}
+        if (link) {
+            mod_length += 1;
+        }
     });
-    for(i=0;i<scale;i++){
-        if(this.c[i]=='1'){
+
+    // initialize this.s
+    for(i = 0; i < scale; i++){
+        if (this.c[i] == "1") {
             this.s.push(parseFloat(1) / mod_length);
-        }else{
+        } else {
             this.s.push(parseFloat(0));
         }
     }
+
 }
 
 // define actions on class N
 N.prototype.on = function(val) {
     this.state += val;
 }
+
 N.prototype.clear = function() {
-    this.state = 0;
+    this.state = 0.0;
+}
+
+// learning function on N
+N.prototype.a = function(){
+    for (i = 0; i < scale; i++) {
+        this.s[i] *= n_dist;
+    }
 }
 
 // firing function on N
 N.prototype.fire = function() {
 
     // check threshold
-    if(this.state >= threshold) {
+    if (this.state >= threshold) {
 
         // state * weight added to neuron
-        for(i=0;i<scale;i++){
-            if(this.c[i]=='1'){
-                click = this.state * this.s[i] + (Math.random() - 1.0) * variation;
+        for (i = 0; i < scale; i++){
+            if (this.c[i] == "1") {
+                click = this.state * this.s[i] + (Math.random()) * variation; 
                 mind[i].on(click);
-                this.clear();
-                this.h += click;
             }
-        }
 
-        // fire neurons just connected
-        for(i=0;i<scale;i++){
-            if(this.c[i]=='1'){
-                mind[i].fire();
-            }
+            // the neuron is now cleared
+            this.clear();
         }
-
 
         // learn for this neuron
         this.a();
 
         // for now, output is printing its key
-        console.log("Firing " + this.key.toString());
+        console.log("Fired: " + this.key);
     }
-}
 
-// learning function on N
-N.prototype.a = function(){
-    // shouldn't really fire immediately, though. Figure this out later.
-    this.state = this.h * n_dist; // just restore the previous thing, I suppose, with ambient weight
 }
 
 // generate network
-for (i=0; i<scale; i++){
-    console.log("Added to mind: neuron #" + keys[i]);
+for (i = 0; i < scale; i++){
     mind.push(new N(i, keys[i]));
 }
+
+console.log("Added total " + scale + " neurons to the network.");
+
 mind.forEach(function(neuron){
     neuron.init();
 });
 
-console.log(mind);
+// define a tick in the neural network
+function tick() {
+    
+    // analogue of a time-unit of processing in the NN
+    
+    // receive input
+    input = "";
+    
+    process.stdin.resume();
+    process.stdin.setEncoding('utf8');
+    process.stdin.on('data', function(data) {
+        input += data;
+        process.stdin.pause();
 
-// begin accepting input
-var input="101000110";
+        // removing the last \n character
+        input = input.substr(0, input.length - 1);
 
-// and then do so every few intervals, whatever that ends up being
-// this event may also be called by a neuron fire
-function take_input(){
-    input = input.split("");
-    // turn on neurons
-    input.forEach(function(bit){
-        mind[parseInt(rkeys[bit])].on(0.8);
-    });
-    // begin firing cascade
-    mind.forEach(function(neuron){
-        neuron.fire();
+        // set neurons from input on
+        input.split("").forEach(function(id){
+            mind[parseInt(rkeys[id])].on(0.8) // this 0.8 is a very arbitrary amount
+       });
+    
+        // check and fire all neurons in the system
+        mind.forEach(function(neuron){
+            neuron.fire();
+        });
+
+        // change n_dist according to output
+        // ...
+
+        console.log(mind);
+        tick();
+        
     });
 }
 
-take_input();
+tick();
 
