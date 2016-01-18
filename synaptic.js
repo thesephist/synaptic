18a66aed8163e4bcce8d6d4c96e2eb8fb0d0ca43
Linus Lee <linus@thelifelongtraveler.com>
2016-01-18 01:42:28 -0500

    Changes to how the simluated World and Tick interact

--- synaptic.js
+++ synaptic.js
@@ -4,12 +4,14 @@
  * Copyright Linus Lee 2015, All rights reserved.
  *
  * For inquires please contact linus@thelifelongtraveler.com
+ *
  * * * * *
+ * 
  * Project outline
  *
  * Event-driven, massively parallel system
- * Changes in N.a: runs after an N.fire, then reflects any learning by filling from ambient right away over the next few intervals
- * There is always a nonzero, exponentially decaying distribution of neurotransmitters
+ *    Changes in N.a: runs after an N.fire, then reflects any learning by filling from ambient right away over the next few intervals
+ *    There is always a nonzero, exponentially decaying distribution of neurotransmitters
  */
 
 // each neuron index associated with its stimulus, KEY
@@ -36,7 +38,7 @@ for (j in keys){
 var supervised = true, // if true, prompt for each input
     input_type = "text", // type of environmental input
     threshold = 2, // firing threshold
-    mind = [], // array of all neurons
+    mind, neuronlist,
     pace = 1.3, // learning pace, with 1 being static; nothing learned
     depreciation_c = 0.8, // how fast individual neurons (and thus the system) loses neurotransmitters
     variation = 0.05, // some preset value
@@ -126,18 +128,48 @@ N.prototype.fire = function() {
 
 }
 
-// generate network
-for (i = 0; i < scale; i++){
-    mind.push(new N(i, keys[i]));
+// mind object
+function Mind (neuronlist) {
+    this.network = neuronlist;
 }
 
+Mind.prototype.init = function() {
+      // init all neurons
+      this.network.forEach(function(neuron){
+          neuron.init();
+      });
+};
+
+Mind.prototype.cycle = function(input) {
+        // set neurons from input on if there is input
+        if (input) {
+            input.split("").forEach(function(id){
+                mind[parseInt(rkeys[id])].on(0.8) // this 0.8 is a very arbitrary amount
+            });
+        }
+        this.network.forEach(function(neuron){
+            neuron.fire();
+        });
+}
+
+Mind.prototype.learn = function(response) {
+    // change n_dist per response
+}
+
+// generate network
+var mindlist = [];
+
+for (i = 0; i < scale; i++){
+    mindlist.push(new N(i, keys[i]));
+}
+mind = new Mind(mindlist);
 console.log("Added total " + scale + " neurons to the network.");
 
-mind.forEach(function(neuron){
-    neuron.init();
-});
+// init network
+mind.init();
 
 // define a tick in the neural network
+// might end up being a method on Mind
 function tick() {
     
     // analogue of a time-unit of processing in the NN
@@ -148,7 +180,7 @@ function tick() {
     process.stdin.resume();
     process.stdin.setEncoding('utf8');
     process.stdin.on('data', function(data) {
-          
+
         // take input from stdin for now
         input += data;
         process.stdin.pause();
@@ -156,24 +188,23 @@ function tick() {
         // removing the last \n character
         input = input.substr(0, input.length - 1);
 
-        // set neurons from input on
-        input.split("").forEach(function(id){
-            mind[parseInt(rkeys[id])].on(0.8) // this 0.8 is a very arbitrary amount
-       });
-    
-        // check and fire all neurons in the system
-        mind.forEach(function(neuron){
-            neuron.fire();
-        });
-
-        // change n_dist according to output
+        // process input in network and fire all neurons
+        mind.cycle(input);
+        
+        // get response from environment
+        // (run world functions simulate() and respond(), with simulate() making the variable and respond() providing the variable
         // ...
 
+        // change n_dist according to output
+        // mind.learn(response);
+
         console.log(mind);
-        tick();
         
     });
 }
+
+// this controls the tick sequences, manages ticks
+var ticker = setInterval(tick, 1000);
 
 // World design as a JS Object
 function World (benefit, harm) {
@@ -185,13 +216,19 @@ function World (benefit, harm) {
     return this;
 }
 
-World.prototype.stimulate(){
-    // provides input to mind
+World.prototype.simulate = function(){
+    
+    console.log("Simulated");
 }
 
-World.prototype.respond(){
-    // provides feedback
+World.prototype.respond = function(){
+
+    console.log("Responded");
 }
+
+// We're implementing a progressive, Iterative Abstraction Leraning (IAL) technique detailed on the ToDo list
 
 tick();
+
+
 
